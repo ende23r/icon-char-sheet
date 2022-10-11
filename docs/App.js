6097bee2d8d2855983dfc79506927dcf2c48942e
Eric <ericandow@gmail.com>
2022-10-11 08:36:31 -0400

    Update proof of concept to incorporate MUI, add subcomponents, use function components, and use hooks.

--- docs/App.js
+++ docs/App.js
@@ -2,30 +2,44 @@
 
 const e = React.createElement;
 
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    const button = React.createElement(
-        'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+function JobSheet(props) {
+    const [loading, setLoading] = React.useState(false);
 
     return e(
-        'span',
-        {},
-        ['Hi!', button]);
-  }
+        MaterialUI.Box,
+        props,
+        ["Here we can talk about the job you chose."]
+    );
+}
+
+class CharacterSheet extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked this.';
+        }
+
+        const button = React.createElement(
+            'button',
+            { onClick: () => this.setState({ liked: true }),
+                key: 'LikeButton1'},
+            'Like'
+        );
+
+        const jobsheet = e(
+            JobSheet,
+            { key: 'jobsheet' },
+            []
+        );
+
+        return [jobsheet, button];
+    }
 }
 
 const domContainer = document.querySelector('#react-root');
 const root = ReactDOM.createRoot(domContainer);
-root.render(e(LikeButton));
+root.render(e(CharacterSheet));
